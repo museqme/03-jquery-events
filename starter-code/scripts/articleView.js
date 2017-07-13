78c66d20d42509c01a88144cf1cfcd8b7becc6aa
Cameron <cameron.32090@gmail.com>
2017-07-12 17:36:36 -0700

    authors list

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -84,5 +84,5 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
-})
+  articleView.populateFilters();
+});
